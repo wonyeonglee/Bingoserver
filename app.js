391a743e50ebec099dc974774993ba00f85f6e6f
wonyeonglee <iwonyeong22@gmail.com>
2021-02-23 14:02:11 +0900

    tokenid 뜨도록 console 출력 됨 -> 근데 pending은 계속 ..

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 var cors = require('cors');
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3002
 
 
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('hi zz sdsdsdsds!')
@@ -15,19 +21,20 @@ const { OAuth2Client } = require('google-auth-library')
 const client = new OAuth2Client(process.env.CLIENT_ID)
 app.post("/api/v1/auth/google", async (req, res) => {
   console.log(req.body);
-  const { token } = req.body
+  const { token } = req.body;
   const ticket = await client.verifyIdToken({
     idToken: token,
     audience: process.env.CLIENT_ID
   });
-  const { name, email, picture } = ticket.getPayload();
-  const user = await db.user.upsert({
-    where: { email: email },
-    update: { name, picture },
-    create: { name, email, picture }
-  })
-  res.status(201)
-  res.json(user)
+  //const { name, email, picture } = ticket.getPayload();
+  // const user = await db.user.upsert({
+  //   where: { email: email },
+  //   update: { name, picture },
+  //   create: { name, email, picture }
+  // })
+  //res.status(201)
+  // res.json(user)
+
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
